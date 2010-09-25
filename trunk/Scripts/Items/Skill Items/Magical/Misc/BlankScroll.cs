9fc97549fe9ef83a80b6528adbd6b449e3bedd46
tonymassey@gmail.com <tonymassey@gmail.com@62005ec2-9939-9381-d33a-b87955d247eb>
2010-09-25 19:35:35 +0000

    Initial Upload of RunUO code.

---
+++ trunk/Scripts/Items/Skill Items/Magical/Misc/BlankScroll.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Server.Items
+{
+	public class BlankScroll : Item, ICommodity
+	{
+		[Constructable]
+		public BlankScroll() : this( 1 )
+		{
+		}
+
+		[Constructable]
+		public BlankScroll( int amount ) : base( 0xEF3 )
+		{
+			Stackable = true;
+			Weight = 1.0;
+			Amount = amount;
+		}
+
+		string ICommodity.Description
+		{
+			get
+			{
+				return String.Format( "blank scroll: {0}", Amount );
+			}
+		}
+
+		int ICommodity.DescriptionNumber { get { return LabelNumber; } }
+		
+		public BlankScroll( Serial serial ) : base( serial )
+		{
+		}
+
+		
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+	}
+}
